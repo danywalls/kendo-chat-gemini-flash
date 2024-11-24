02ea1de6c169ea48310f8d7e768ee4da3e7e7bff
Dany Paredes <danywalls4@gmail.com>
2024-11-24 17:49:10 +0100

    feat(app.component): add the KENDO_UI component with template for upload
    add the kendo_ui to allow upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {KENDO_CHAT} from '@progress/kendo-angular-conversational-ui';
+import {Component, signal} from '@angular/core';
+import {KENDO_CHAT, Message, SendMessageEvent, User} from '@progress/kendo-angular-conversational-ui';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,24 @@ import {KENDO_CHAT} from '@progress/kendo-angular-conversational-ui';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'chatlens';
+
+
+  user : User = {
+    id: 2,
+    name: 'John Doe',
+  }
+
+  messages = signal<Message[]>([
+    {
+      text: 'Hello! how I can help you?',
+      author: {
+        id: 1,
+        name: 'Gemini'
+      }
+    }
+  ])
+
+  addMessage(message : SendMessageEvent) {
+    this.messages.update((m => [...m, message.message]));
+  }
 }
